b52b41c58ab455c0c3a5b3aba4c7f53513781aef
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-06-27 16:51:52 +0100

    mealItem form

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,19 @@
+import classes from "./MealItemForm.module.css"
+import Input from "../../UI/Input";
+
+const MealItemForm = props => {
+    return (
+        <form className={classes.form}>
+            <Input label={'Amount'} input={{
+                id: 'amount',
+                type: 'number',
+                min: '1',
+                max: '5',
+                defaultValue: '1'
+            }} />
+            <button className={classes.button}>+ Add</button>
+        </form>
+    )
+};
+
+export default MealItemForm;
