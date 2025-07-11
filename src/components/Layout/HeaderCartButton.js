be505055cdc4ccf77c363b9880f9b5fd58a448a5
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-07-11 15:49:37 +0100

    building the cartItem actions

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
+import CartContext from "../../store/Cart-context";
 
 const HeaderCartButton = props => {
+    const CartCtx = useContext(CartContext);
+
+    const numberOfCartItems = CartCtx.items.reduce((curNumber, item)=>{
+        return curNumber + item.amount;
+    }, 0);
+
     return <div>
         <button className={classes.button} onClick={props.openCart} >
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge} >3</span>
+            <span className={classes.badge} >{numberOfCartItems}</span>
         </button>
     </div>
         
