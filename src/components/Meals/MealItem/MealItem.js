846686d10a0566facc0027a436982669097e72a9
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-07-31 20:11:10 +0100

    implementation of cart item increase when Add button is clicked

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
 import classes from "./MealItem.module.css"
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/Cart-context";
 
 const MealItems = props => {
+  const cartCxt = useContext(CartContext)
+
     const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCxt.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
@@ -12,7 +25,7 @@ const MealItems = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     )
